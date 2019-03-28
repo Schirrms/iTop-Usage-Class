f6b8df1a931519b2571f76ddfe549e35d7cf033f
Schirrms <schirrms@schirrms.net>
2019-03-28 22:01:31 +0100

    Initial push on git - version 1.0.0

---
+++ schirrms-usage-class/en.dict.schirrms-usage-class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2019 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:Usage' => 'Usage (Prod/Int/Dev...)',
+	'Class:Usage/Attribute:name' => 'Usage',
+	'Class:Usage/Attribute:functional_cis_list' => 'Items',
+	'Class:FunctionalCI/Attribute:usage_id' => 'Usage',
+));
+?>
