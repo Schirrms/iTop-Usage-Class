20f34c7e45cc8be1be6fe4282baaac037a9c9c9b
Schirrms <schirrms@schirrms.net>
2019-04-01 15:56:59 +0200

    release 1.0.1 - Add a org_id field

--- schirrms-usage-class/en.dict.schirrms-usage-class.php
+++ schirrms-usage-class/en.dict.schirrms-usage-class.php
@@ -10,6 +10,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
 	'Class:Usage' => 'Usage (Prod/Int/Dev...)',
 	'Class:Usage/Attribute:name' => 'Usage',
+	'Class:Usage/Attribute:org_id' => 'Organization',
 	'Class:Usage/Attribute:functional_cis_list' => 'Items',
 	'Class:FunctionalCI/Attribute:usage_id' => 'Usage',
 ));
